6d9fe2ff43ea76c3a3a7b8699bb73d22f1b216b8
John Ross <johnyboy11@hotmail.co.uk>
2021-04-09 17:15:16 +0100

    The room has a name

--- tests/test_room.py
+++ tests/test_room.py
@@ -5,4 +5,7 @@ class TestRoom(unittest.TestCase):
 
     def setUp(self):
 
-        self.room = "The Real Funky Room"
+        self.room = Room("The Real Funky Room")
+
+    def test_room_has_a_name(self):
+        self.assertEqual("The Real Funky Room", self.room.name)
