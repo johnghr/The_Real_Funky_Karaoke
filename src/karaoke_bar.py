ace0b866344666f4aa0d4ad2fb9f42677b674ce7
John Ross <johnyboy11@hotmail.co.uk>
2021-04-11 17:43:24 +0100

    Can add multiple songs to playlist

--- src/karaoke_bar.py
+++ src/karaoke_bar.py
@@ -1,17 +1,15 @@
-from src.room import Room
-from src.guest import Guest
-from src.song import Song
-
-
-
 def guest_check_in(room, guest):
-    if len(room.guests_and_songs["guest_list"]) < 3:
-        room.guests_and_songs["guest_list"].append(guest)
-    else:
-        return "Sorry cool cats, this Karaoke ain't funky enough for four!"
+        if len(room.guests_and_songs["guest_list"]) < 3:
+            room.guests_and_songs["guest_list"].append(guest)
+        else:
+            return "Sorry cool cats, this Karaoke ain't funky enough for four!"
 
 def add_to_playlist(room, song):
     room.guests_and_songs["playlist"].append(song)
+
+def add_multiple_songs_to_playlist(room, songs):
+    for song in songs:
+        room.guests_and_songs["playlist"].append(song)
 
 def remove_guest(room, guest):
     room.guests_and_songs["guest_list"].remove(guest)
@@ -21,8 +19,18 @@ def charge_entry_fee(guest):
 
 def check_for_favorite_song(room, guest):
     if room.guests_and_songs["playlist"][0].name == guest.favorite_song:
-        return "Damn thats Funky!"
+            return "Damn thats Funky!"
    
+def group_booking(room, group):
+    for guest in group:
+        if len(group) > 4:
+            return "Sorry cool cats, this Karaoke ain't funky enough for five!"
+        else:
+            room.guests_and_songs["guest_list"].append(guest)
+            
+        
+            
+                
     
 
 
--- tests/test_karaoke_bar.py
+++ tests/test_karaoke_bar.py
@@ -14,12 +14,26 @@ class TestKaraokeBar(unittest.TestCase):
         self.guest_2 = Guest("Bootsy Collins", 40.00, "All around the world")
         self.guest_3 = Guest("Stevie Wonder", 60.00, "By the way")
         self.guest_4 = Guest("George Clinton", 70.00, "Give it away")
+        self.guest_5 = Guest("Robert 'Kool' Bell", 80.00, "Aeroplane")
+        self.group = []
+        self.group.append(self.guest_1) 
+        self.group.append(self.guest_2) 
+        self.group.append(self.guest_3)
+        self.group.append(self.guest_4)
+        self.group_2 = []
+        self.group_2.append(self.guest_1) 
+        self.group_2.append(self.guest_2) 
+        self.group_2.append(self.guest_3)
+        self.group_2.append(self.guest_4)
+        self.group_2.append(self.guest_5)
+        self.songs = []
+        self.songs.append(self.song_1)
+        self.songs.append(self.song_2)
+
 
     def test_karaoke_can_check_guest_into_room(self):
         guest_check_in(self.room, self.guest_1)
-        guest_check_in(self.room, self.guest_2)
-        guest_check_in(self.room, self.guest_3)
-        self.assertEqual(3, len(self.room.guests_and_songs["guest_list"]))
+        self.assertEqual(1, len(self.room.guests_and_songs["guest_list"]))
 
     def test_karaoke_can_add_song_to_playlist(self):
         add_to_playlist(self.room, self.song_1)
@@ -46,5 +60,20 @@ class TestKaraokeBar(unittest.TestCase):
         guest_check_in(self.room, self.guest_1)
         add_to_playlist(self.room, self.song_2)
         self.assertEqual("Damn thats Funky!", check_for_favorite_song(self.room, self.guest_1))
+
+    def test_group_booking(self):
+        group_booking(self.room, self.group)
+        self.assertEqual(4, len(self.room.guests_and_songs["guest_list"]))
+
+    def test_group_booking_has_capacity(self):
+        self.assertEqual("Sorry cool cats, this Karaoke ain't funky enough for five!", group_booking(self.room, self.group_2))
+
+    def test_add_multiple_songs_to_playlist(self):
+        add_multiple_songs_to_playlist(self.room, self.songs)
+        self.assertEqual(2, len(self.room.guests_and_songs["playlist"]))
+    
+    # def test_guest_in_group_cheers_favorite_song(self):
+
+        
 
    
