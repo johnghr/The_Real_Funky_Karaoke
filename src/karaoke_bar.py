7dea6901c73941549e5335c1d0dbf4b9a15c3989
John Ross <johnyboy11@hotmail.co.uk>
2021-04-11 12:50:33 +0100

    Karaoke has capacity

--- src/karaoke_bar.py
+++ src/karaoke_bar.py
@@ -7,7 +7,10 @@ room = Room("The Real Funky Room")
 song = Song("Get Up Offa That Thing")
 
 def guest_check_in(room, guest):
-    room.guests_and_songs["guest_list"].append(guest)
+    if len(room.guests_and_songs["guest_list"]) < 3:
+        room.guests_and_songs["guest_list"].append(guest)
+    else:
+        return "Sorry cool cats, this Karaoke ain't funky enough for four of you!"
 
 def add_to_playlist(room, song):
     room.guests_and_songs["playlist"].append(song)
--- tests/test_karaoke_bar.py
+++ tests/test_karaoke_bar.py
@@ -9,11 +9,16 @@ class TestKaraokeBar(unittest.TestCase):
     def setUp(self):
         self.song = Song("Get Up Offa That Thing")
         self.room = Room("The Real Funky Room")
-        self.guest = Guest("James Brown")
+        self.guest_1 = Guest("James Brown")
+        self.guest_2 = Guest("Bootsy Collins")
+        self.guest_3 = Guest("Stevie Wonder")
+        self.guest_4 = Guest("George Clinton")
 
     def test_karaoke_can_check_guest_into_room(self):
-        guest_check_in(self.room, self.guest)
-        self.assertEqual(1, len(self.room.guests_and_songs["guest_list"]))
+        guest_check_in(self.room, self.guest_1)
+        guest_check_in(self.room, self.guest_2)
+        guest_check_in(self.room, self.guest_3)
+        self.assertEqual(3, len(self.room.guests_and_songs["guest_list"]))
 
     def test_karaoke_can_add_song_to_playlist(self):
         add_to_playlist(self.room, self.song)
@@ -21,9 +26,16 @@ class TestKaraokeBar(unittest.TestCase):
         self.assertEqual("Get Up Offa That Thing", self.room.guests_and_songs["playlist"][0].name)
 
     def test_karaoke_can_remove_guest(self):
-        guest_check_in(self.room, self.guest)
-        remove_guest(self.room, self.guest)
+        guest_check_in(self.room, self.guest_1)
+        remove_guest(self.room, self.guest_1)
         self.assertEqual(0, len(self.room.guests_and_songs["guest_list"]))
+
+    def test_karaoke_bar_has_capacity_limit(self):
+        guest_check_in(self.room, self.guest_1)
+        guest_check_in(self.room, self.guest_2)
+        guest_check_in(self.room, self.guest_3)
+        guest_check_in(self.room, self.guest_4)
+        self.assertEqual(3, len(self.room.guests_and_songs["guest_list"]))
 
 
 
